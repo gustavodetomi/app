943d22609c25befa3f87393c7991286119b60262
Gustavo Detomi <detomigustavo@gmail.com>
2021-03-13 17:12:38 -0300

    PWA

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function calc(){
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
+function calc() {
     let n1 = document.getElementById('n1');
     let n2 = document.getElementById('n2');
     let n3 = document.getElementById('n3');
-    console.log(n1.value)
-    console.log(n2.value)
     n3.value = Number(n1.value) + Number(n2.value)
 }
