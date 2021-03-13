f7927a03e2bf594f2b4ad7259c79dafc5039bd7a
Gustavo Detomi <detomigustavo@gmail.com>
2021-03-13 17:31:13 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
 function calc() {
     let n1 = document.getElementById('n1');
     let n2 = document.getElementById('n2');
